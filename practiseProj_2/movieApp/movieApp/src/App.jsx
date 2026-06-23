783152f157f76dbf71b9699d1efe814d62a7b4a9
AffanA-1 <mdaffanaslam@cloudkaptan.com>
2026-06-23 10:47:32 +0530

    Feat: Added Link type Routing as well - Through Navbar

--- practiseProj_2/movieApp/movieApp/src/App.jsx
+++ practiseProj_2/movieApp/movieApp/src/App.jsx
@@ -2,10 +2,12 @@ import './css/App.css'
 import Favorites from './pages/Favorites'
 import Home from './pages/Home'
 import {Routes, Route} from "react-router-dom"
+import NavBar from './components/NavBar'
 
 function App() {
 
   return <>
+  <NavBar />
   <main className='main-content'>
     {/* Step2 for Adidng Route - Add the Routings */}
     <Routes>
