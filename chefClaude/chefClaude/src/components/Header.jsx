9d4554f7bf3b8442b08c346e8c78dc68c447de8d
AffanA-1 <mdaffanaslam@cloudkaptan.com>
2026-06-24 19:27:15 +0530

    Feat: New Project ChefClaude started

---
+++ chefClaude/chefClaude/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../css/Header.css'
+
+function Header(){
+
+    return (<>
+    <header>
+        <img src='https://t4.ftcdn.net/jpg/02/72/65/95/360_F_272659528_4BeXAVkr69LHfeTMEaEaVEFBco5ooFYj.jpg' alt="Chef Claude Logo" />
+        <h1>Chef Claude</h1>
+        </header>
+    </>)
+
+}
+
+export default Header
---
+++ chefClaude/chefClaude/src/components/Main.jsx
@@ -0,0 +1,47 @@
+import "../css/Main.css"
+import { useState } from "react"
+
+function Main(){
+
+    const [ingredients, setIngredients] = useState([])
+    const [ingredientInput, setIngredientInput] = useState()
+
+   const ingredientListItems = ingredients.map((ing, index) => 
+        <li className="list-items" key={`${ing}-${index}`}>{ing}</li>
+    )
+
+
+
+    // form itself doesnt has value, the input has the value, so onchange we are fetchingf that value
+    function handleOnChange(e) {
+        setIngredientInput(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        if (ingredientInput.trim() !== "") {
+            setIngredients(prev => [...prev, ingredientInput]);
+            setIngredientInput("");
+        }
+    }
+
+    return (<>
+        <main>
+            <div className="content-container">
+            <form onSubmit={handleSubmit} className="search-Form">
+
+                <input type="text"  className="ingredient-input" placeholder="e.g Oregano" value={ingredientInput} onChange={handleOnChange}/>
+                <button className="search-btn" >Add Ingredients</button>
+
+            </form>
+
+            <ul>
+                {ingredientListItems}
+            </ul>
+        </div>
+        </main>
+    </>)
+}
+
+
+export default Main
